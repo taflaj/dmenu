12afe345995aafc2b6493d2de61edd9d20acfd8a
José Tafla <jose@tafla.com>
2023-09-07 12:06:12 -0700

    Expanded minimum window width.

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static const unsigned int alpha = 0xaa;     /* Amount of opacity. 0xff is opaque             */
 static int centered = 0;                    /* -c option; centers dmenu on screen            */
-static int min_width = 500;                 /* minimum width when centered                   */
+static int min_width = 1000;                 /* minimum width when centered                   */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
